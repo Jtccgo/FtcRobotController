095631104e28318049dc6d1c876771d353d5cf31
Jason <jasontc2099@icloud.com>
2026-09-21 16:33:29 -0500

    added time methods and a framework for the controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotCentric.java
@@ -6,25 +6,43 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "RobotCentric", group = "drive")
 public class RobotCentric extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private double savedTime = 0;
     private DcMotorEx flm;
     private DcMotorEx frm;
     private DcMotorEx blm;
     private DcMotorEx brm;
+    private DcMotorEx chamberMotor;//controls the cylinders that swivel in the center of the robot
+    private DcMotorEx intakeMotor;//connects to the shaft with several compliant wheels
+    private DcMotorEx pushServo;//positioned over the swivel cylinders to push the ball forwards into the flywheels
+    private DcMotorEx leftFlywheel;
+    private DcMotorEx rightFlywheel;
 
-    private Servo ex;
-
+    //relative to the robot rotated with wheels facing forwards
     @Override
     public void runOpMode() {
-
+        //wheel motors
         flm = hardwareMap.get(DcMotorEx.class, "fl");
         frm = hardwareMap.get(DcMotorEx.class, "fr");
         frm.setDirection(DcMotorEx.Direction.REVERSE);
         blm = hardwareMap.get(DcMotorEx.class, "bl");
         brm = hardwareMap.get(DcMotorEx.class, "br");
-
+        //intake & outtake
+        intakeMotor = hardwareMap.get(DcMotorEx.class, "intake");
+        chamberMotor = hardwareMap.get(DcMotorEx.class, "chamber");
+        pushServo = hardwareMap.get(DcMotorEx.class, "push");
+        leftFlywheel = hardwareMap.get(DcMotorEx.class, "lfly");
+        rightFlywheel= hardwareMap.get(DcMotorEx.class, "rfly");
+        //motor modes
+        rightFlywheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftFlywheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        pushServo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        chamberMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         flm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         blm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -39,6 +57,11 @@ public class RobotCentric extends LinearOpMode {
 
         // 4. Active Driver Control Loop
         while (opModeIsActive()) {
+            runtime.startTime();
+            //default controls(like what i choose)
+            //driver controls
+            intakeControls();
+            outtakeControls();
             double straight = -gamepad1.left_stick_y;//inverse because 1.0 is down
             double strafe = gamepad1.left_stick_x;
             double rotate = gamepad1.right_stick_x;
@@ -67,5 +90,30 @@ public class RobotCentric extends LinearOpMode {
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
+    }
+    public void intakeControls() {
+        leftFlywheel.setPower(gamepad1.left_trigger);
+        rightFlywheel.setPower(gamepad1.right_trigger);
+        if (gamepad1.right_bumper) {
+
+            pushServo.setTargetPosition(1);
+        }
+        if (runtime.seconds() > 1.0) {
+        }
+        pushServo.setTargetPosition(0);
+    }
+    public void outtakeControls() {
+
+
+    }
+    private double timeSinceLastSave() {
+        return runtime.seconds() - savedTime;
+    }
+    private void saveTime() {
+        savedTime = runtime.seconds();
+    }
+    private void resetTime() {
+        runtime.reset();
+        savedTime = 0;
     }
 }
